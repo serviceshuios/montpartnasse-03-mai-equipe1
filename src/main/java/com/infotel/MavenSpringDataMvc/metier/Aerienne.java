29152589af7123ca41c55f542d6eff09dcc99d63
Soulaima MEZGHANI <soulayma.mezghani@gmail.com>
2019-05-06 17:41:52 +0200

    Aerienne

--- src/main/java/com/infotel/MavenSpringDataMvc/metier/Aerienne.java
+++ src/main/java/com/infotel/MavenSpringDataMvc/metier/Aerienne.java
@@ -1,3 +1,6 @@
+/**
+ * ce package consiste à presenter la classe Aerienne 
+ */
 package com.infotel.MavenSpringDataMvc.metier;
 
 import javax.persistence.DiscriminatorValue;
@@ -5,7 +8,11 @@ import javax.persistence.Entity;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
+/**
+ * 
+ * @author IN-MP-023
+ *
+ */
 @Entity
 @DiscriminatorValue("AER")
 @Component
@@ -13,15 +20,32 @@ import org.springframework.stereotype.Component;
 public class Aerienne extends Cargaison{
 	private String numVol;
 	private double taxe;
+	/**
+	 *  cette methode permet de retourner le numero du vol
+     *     @return renvoie le num de vol
+	
+	 */
 	public String getNumVol() {
 		return numVol;
 	}
+	/**
+	 * cette methode permet de setter le num de vol
+	 * @param numVol le numero du vol
+	 */
 	public void setNumVol(String numVol) {
 		this.numVol = numVol;
 	}
+	/**
+	 * cette methode permet de retourner le taxe
+	 * @return renvoie le taxe
+	 */
 	public double getTaxe() {
 		return taxe;
 	}
+	/**
+	 * cette methode permet de setter le txe
+	 * @param taxe le taxe 
+	 */
 	public void setTaxe(double taxe) {
 		this.taxe = taxe;
 	}
